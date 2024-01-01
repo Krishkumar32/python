577ef94d5253e7a7700b6a951aa5f80248c1b44f
Krishkumar32 <154981848+Krishkumar32@users.noreply.github.com>
2024-01-02 02:18:35 +0530

    Create listiteration.py

---
+++ listiteration.py
@@ -0,0 +1,19 @@
+l=[10,20,30,40,50]
+t=len(l) # this line show the length of l
+print(t)
+
+print()
+for a in range(t): #through range we can show the length
+    print(l[a])
+    print("")
+    
+for a in l:
+    print(a)
+    
+    print()
+    # reverse
+l=[10,20,30,40,50]
+t=len(l)
+for a in range(t-1,-1,-1):
+  print(l[a])
+    
