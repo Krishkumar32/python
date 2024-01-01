06b568089a5081c8a0718d1f048933f01bfee1d9
Krishkumar32 <154981848+Krishkumar32@users.noreply.github.com>
2024-01-02 01:27:42 +0530

    Create strformat.py

---
+++ strformat.py
@@ -0,0 +1,8 @@
+w="welcome {} to {} Wscubetech".format("hello",20); #hello string should add in first curly barcket and 20 should add in second curly bracket
+print(w)
+
+w="welcome {0} to {1} Wscubetech".format("hello",20); 
+print(w)
+
+w="welcome {b:<10} to {a:^10} Wscubetech".format(a=30,b=40) 
+print(w)
