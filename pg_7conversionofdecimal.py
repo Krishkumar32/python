14ad32ce45d89ac73c1117f5b345d329157ada04
Krishkumar32 <154981848+Krishkumar32@users.noreply.github.com>
2024-01-14 04:47:11 +0530

    Update pg_7conversionofdecimal.py

--- pg_7conversionofdecimal.py
+++ pg_7conversionofdecimal.py
@@ -1,6 +1,6 @@
 decimal =int(input("enter a number here: "))
 
-print("the cinversion of decimal number",decimal,"is: ")
+print("the conversion of decimal number",decimal,"is: ")
 print(bin(decimal),"in binary")
 print(bin(decimal),"in octal")
 print(hex(decimal),"in hexadecimal")
