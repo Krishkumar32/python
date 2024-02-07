f755e56fc54f6335c28526dedbbb8982c0570776
Krishkumar32 <154981848+Krishkumar32@users.noreply.github.com>
2024-02-07 05:33:46 +0530

    Create pg_18pythonsyntax.py

---
+++ pg_pythonsyntax.py
@@ -0,0 +1,19 @@
+'''Python Indentation:
+Indentation refers to the spaces at the beginning of a code line.
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+Python uses indentation to indicate a block of code.'''
+
+#example:
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+'''Python Variables:
+In Python, variables are created when you assign a value to it'''
+#example
+x=5
+y="hello world"
+print(x)
+print(y)
+
